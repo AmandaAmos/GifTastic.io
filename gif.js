663f878e32c36f9e810340f0737480c2260f6bb1
Amanda Amos <59996872+AmandaAmos@users.noreply.github.com>
2020-03-11 23:53:55 -0700

    working on homework.

---
+++ gif.js
@@ -0,0 +1,20 @@
+//opening code to call function
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</link>
+
+for ()
+//create the API call to Giphy
+        //1)create parameters for requested data
+                //a)rating
+                //b)still of gif
+                //c)action gifs
+        //2)use API key with a generic variable that can be looped through to pull each keyword
+
+
+//display ten results for each item on the original array from giphy
+        //
+
+//display users input as a new button with giphy api
