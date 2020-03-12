14dc6e0193889fc290c82acd3b6b79163fcddb5f
Amanda Amos <59996872+AmandaAmos@users.noreply.github.com>
2020-03-12 14:03:08 -0700

    more stuff

--- gif.js
+++ gif.js
@@ -1,11 +1,18 @@
 //opening code to call function
 
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"
-integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</link>
 
-for ()
+
+var = Flowers {
+    salvia
+    dande
+    lilies  
+}
+
+object.onclick function(){
+    chosenFlower
+    console.log(chosenFlower);
+}
+
 //create the API call to Giphy
         //1)create parameters for requested data
                 //a)rating
@@ -18,3 +25,7 @@ for ()
         //
 
 //display users input as a new button with giphy api
+
+
+
+giphy api key 748mmYRkwy1N1kj7EL06Yg0vfcUXLffS
